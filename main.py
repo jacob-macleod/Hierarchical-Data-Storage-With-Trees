16b2b7a4627729edfaeb65813a27557c1521b39b
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-01-12 13:43:07 +0000

    Added more code

--- main.py
+++ main.py
@@ -9,9 +9,28 @@ def findNodeName(nodeNumber) :
             if int(node.split(",")[1]) < nodeNumber:
                 rootNode = int(node.split(",")[1])
 
-    # Search this node in mainData.csv
+    # Find node in mainData.csv
     with open("mainData.csv", "r") as mainData:
         nodes = mainData.readlines()
         return nodes[nodeNumber].split(",")[0]
 
-print(findNodeName(5))
+def listChildrenOfNode(nodeNumber) :
+    rootNode = 1
+    # Decide which root to start at
+    with open("rootNodesIndex.csv", "r") as rootNodes:
+        # Find the biggest root node which is not bigger than nodeNumber
+        # This will be the root the nodeNumber belongs to
+        for node in rootNodes:
+            if int(node.split(",")[1]) < nodeNumber:
+                rootNode = int(node.split(",")[1])
+
+    # Find node in mainData.csv
+    with open("mainData.csv", "r") as mainData:
+        nodes = mainData.readlines()
+        parent = nodes[nodeNumber].split(",")
+
+        # Find the children of the parent
+        for i in range(2, len(parent)):
+            print (parent[i])
+
+print(listChildrenOfNode(2))
